fbfe2e152b42dd6e7e6b1fe6f03daf9d09138a08
AndreiSold <sold_andrei@yahoo.com>
2018-11-02 17:26:09 +0200

    Started to secure the application with OAuth 2

--- src/main/java/ro/msg/learning/shop/configurations/WebSecurityConfiguration.java
+++ src/main/java/ro/msg/learning/shop/configurations/WebSecurityConfiguration.java
@@ -1,109 +1,109 @@
-package ro.msg.learning.shop.configurations;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import ro.msg.learning.shop.entities.Customer;
-import ro.msg.learning.shop.entities.Role;
-import ro.msg.learning.shop.exceptions.CustomerIdNotFoundException;
-import ro.msg.learning.shop.repositories.CustomerRepository;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
-
-@EnableWebSecurity
-@Configuration
-@RequiredArgsConstructor
-@Slf4j
-@ComponentScan("ro.msg.learning.shop.services")
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private final CustomerRepository customerRepository;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.userDetailsService(s -> {
-
-            final Customer customer = customerRepository.findByUsername(s);
-
-            if (customer == null) {
-                log.error("Given customer username does not exist!");
-                throw new CustomerIdNotFoundException("This username does not exist!");
-            }
-
-            return new UserDetails() {
-                @Override
-                public Collection<? extends GrantedAuthority> getAuthorities() {
-                    return customer.getRoles().parallelStream().map(Role::getName).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-                }
-
-                @Override
-                public String getPassword() {
-                    return customer.getPassword();
-                }
-
-                @Override
-                public String getUsername() {
-                    return s;
-                }
-
-                @Override
-                public boolean isAccountNonExpired() {
-                    return true;
-                }
-
-                @Override
-                public boolean isAccountNonLocked() {
-                    return true;
-                }
-
-                @Override
-                public boolean isCredentialsNonExpired() {
-                    return true;
-                }
-
-                @Override
-                public boolean isEnabled() {
-                    return true;
-                }
-            };
-        });
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/customer").permitAll()
-            .antMatchers("/customer/profile").authenticated()
-            .antMatchers("/customer/profile/*").authenticated()
-            .antMatchers("/customer/register").permitAll()
-            .antMatchers("/stocks/export-stocks-from-location/*").hasRole("ADMINISTRATOR")
-            .antMatchers(HttpMethod.GET).permitAll()
-            .antMatchers(HttpMethod.POST).authenticated()
-            .antMatchers(HttpMethod.DELETE, "/customer/*").hasRole("ADMINISTRATOR")
-            .anyRequest().denyAll()
-            .and()
-            .httpBasic();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-}
+//package ro.msg.learning.shop.configurations;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import ro.msg.learning.shop.entities.Customer;
+//import ro.msg.learning.shop.entities.Role;
+//import ro.msg.learning.shop.exceptions.CustomerIdNotFoundException;
+//import ro.msg.learning.shop.repositories.CustomerRepository;
+//
+//import java.util.Collection;
+//import java.util.stream.Collectors;
+//
+//@EnableWebSecurity
+//@Configuration
+//@RequiredArgsConstructor
+//@Slf4j
+//@ComponentScan("ro.msg.learning.shop.services")
+//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    private final CustomerRepository customerRepository;
+//
+////    @Override
+////    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+////
+////        auth.userDetailsService(s -> {
+////
+////            final Customer customer = customerRepository.findByUsername(s);
+////
+////            if (customer == null) {
+////                log.error("Given customer username does not exist!");
+////                throw new CustomerIdNotFoundException("This username does not exist!");
+////            }
+////
+////            return new UserDetails() {
+////                @Override
+////                public Collection<? extends GrantedAuthority> getAuthorities() {
+////                    return customer.getRoles().parallelStream().map(Role::getName).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+////                }
+////
+////                @Override
+////                public String getPassword() {
+////                    return customer.getPassword();
+////                }
+////
+////                @Override
+////                public String getUsername() {
+////                    return s;
+////                }
+////
+////                @Override
+////                public boolean isAccountNonExpired() {
+////                    return true;
+////                }
+////
+////                @Override
+////                public boolean isAccountNonLocked() {
+////                    return true;
+////                }
+////
+////                @Override
+////                public boolean isCredentialsNonExpired() {
+////                    return true;
+////                }
+////
+////                @Override
+////                public boolean isEnabled() {
+////                    return true;
+////                }
+////            };
+////        });
+////    }
+////
+////    @Override
+////    protected void configure(HttpSecurity http) throws Exception {
+////        http
+////            .csrf().disable()
+////            .authorizeRequests()
+////            .antMatchers("/customer").permitAll()
+////            .antMatchers("/customer/profile").authenticated()
+////            .antMatchers("/customer/profile/*").authenticated()
+////            .antMatchers("/customer/register").permitAll()
+////            .antMatchers("/stocks/export-stocks-from-location/*").hasRole("ADMINISTRATOR")
+////            .antMatchers(HttpMethod.GET).permitAll()
+////            .antMatchers(HttpMethod.POST).authenticated()
+////            .antMatchers(HttpMethod.DELETE, "/customer/*").hasRole("ADMINISTRATOR")
+////            .anyRequest().denyAll()
+////            .and()
+////            .httpBasic();
+////    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//}
---
+++ src/main/java/ro/msg/learning/shop/security/OAuth2SecurityConfiguration.java
@@ -0,0 +1,124 @@
+package ro.msg.learning.shop.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import ro.msg.learning.shop.entities.Customer;
+import ro.msg.learning.shop.entities.Role;
+import ro.msg.learning.shop.exceptions.CustomerIdNotFoundException;
+import ro.msg.learning.shop.repositories.CustomerRepository;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final ClientDetailsService clientDetailsService;
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("bill").password("1234").roles("ADMINISTRATOR").and()
+            .withUser("bob").password("1234").roles("CUSTOMER");
+
+//        auth.userDetailsService(s -> {
+//            final Customer customer = customerRepository.findByUsername(s);
+//            if (customer == null) {
+//                log.error("Given customer username does not exist!");
+//                throw new CustomerIdNotFoundException("This username does not exist!");
+//            }
+//            return new UserDetails() {
+//                @Override
+//                public Collection<? extends GrantedAuthority> getAuthorities() {
+//                    return customer.getRoles().parallelStream().map(Role::getName).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+//                }
+//                @Override
+//                public String getPassword() {
+//                    return customer.getPassword();
+//                }
+//                @Override
+//                public String getUsername() {
+//                    return s;
+//                }
+//                @Override
+//                public boolean isAccountNonExpired() {
+//                    return true;
+//                }
+//                @Override
+//                public boolean isAccountNonLocked() {
+//                    return true;
+//                }
+//                @Override
+//                public boolean isCredentialsNonExpired() {
+//                    return true;
+//                }
+//                @Override
+//                public boolean isEnabled() {
+//                    return true;
+//                }
+//            };
+//        });
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .anonymous().disable()
+            .authorizeRequests()
+            .antMatchers("/oauth/token").permitAll();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+    @Bean
+    @Autowired
+    public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore) {
+        TokenStoreUserApprovalHandler handler = new TokenStoreUserApprovalHandler();
+        handler.setTokenStore(tokenStore);
+        handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService));
+        handler.setClientDetailsService(clientDetailsService);
+        return handler;
+    }
+
+    @Bean
+    @Autowired
+    public ApprovalStore approvalStore(TokenStore tokenStore) throws Exception {
+        TokenApprovalStore store = new TokenApprovalStore();
+        store.setTokenStore(tokenStore);
+        return store;
+    }
+
+}
